f15e94728ae0c22449b9b397141637109159548b
fabim1992 <fabioalbuquerquedev@gmail.com>
2023-11-06 22:15:05 -0300

    test

---
+++ lib/pages/assist_cards.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AssistCard extends StatefulWidget {
+  const AssistCard({super.key});
+
+  @override
+  State<AssistCard> createState() => _AssistCardState();
+}
+
+class _AssistCardState extends State<AssistCard> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
